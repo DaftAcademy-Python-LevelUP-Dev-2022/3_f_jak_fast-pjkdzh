89858eb333939d5419d6199b3ba425f13b679be6
pjkdzh <ewelina_banach@interia.eu>
2022-05-31 00:48:17 +0200

    task3.2

--- app.py
+++ app.py
@@ -23,8 +23,9 @@ def start():
     """
 
 def get_current_username(credentials: HTTPBasicCredentials = Depends(security)):
-    correct_date = secrets.compare_digest(credentials.password, "swordfish")
-    if (correct_date):
+    correct_username = secrets.compare_digest(credentials.username, "stanleyjobson")
+    correct_password = secrets.compare_digest(credentials.password, "swordfish")
+    if not (correct_username and correct_password):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Incorrect email or password",
