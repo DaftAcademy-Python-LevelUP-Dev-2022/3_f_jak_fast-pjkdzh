d862b4368623bdad932bcd1eb6e1610cd996c3a8
pjkdzh <ewelina_banach@interia.eu>
2022-05-31 00:07:51 +0200

    task3.2

--- app.py
+++ app.py
@@ -2,39 +2,17 @@ import datetime
 import secrets
 
 from fastapi import Depends, FastAPI, HTTPException, status
-from fastapi.security import HTTPBasic, HTTPBasicCredentials
-
-app = FastAPI()
-
-security = HTTPBasic()
-
-
-def get_current_username(credentials: HTTPBasicCredentials = Depends(security)):
-    correct_username = secrets.compare_digest(credentials.username, "stanleyjobson")
-    correct_password = secrets.compare_digest(credentials.password, "swordfish")
-    if not (correct_username and correct_password):
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect email or password",
-            headers={"WWW-Authenticate": "Basic"},
-        )
-    return credentials.username
-
-
-@app.get("/users/me")
-def read_current_user(username: str = Depends(get_current_username)):
-    return {"username": username}
-
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 
 app = FastAPI()
+
+security = HTTPBasic()
 templates = Jinja2Templates("""<html>
         <h1>Welcome {{name}}! You are {{age}}</h1>
 </html>""")
-security = HTTPBasic()
 
 
 @app.get("/start", response_class=HTMLResponse)
@@ -57,7 +35,7 @@ def get_current_username(credentials: HTTPBasicCredentials = Depends(security)):
 
 @app.get("/check")
 def read_current_user(credentials: HTTPBasicCredentials = Depends(security)):
-    return {"username": credentials.username, "password": credentials.password}
+    return templates.TemplateResponse({"name": credentials.username, "age": datetime.date.today() - datetime.date(credentials.username)})
 #
 # @app.get("/check", response_class=HTMLResponse)
 # def check(name: str, date: str):
