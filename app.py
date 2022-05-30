f67ad7bf78bc2cd4dccd0b94616517b62b048711
pjkdzh <ewelina_banach@interia.eu>
2022-05-30 23:02:08 +0200

    task3.1

--- app.py
+++ app.py
@@ -5,9 +5,13 @@ app = FastAPI()
 
 
 @app.get("/start", response_class=HTMLResponse)
-def index_static():
+def start():
     return """
     <html>
         <h1>The unix epoch started at 1970-01-01</h1>
     </html>
     """
+
+@app.post("/check/{name}/{date}", response_class=HTMLResponse)
+def check(name: str, date: date):
+    return {"sample_variable": sample_variable}
