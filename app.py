ed0832d37103aa8e9c2040959f9b1380e4dc5ec7
pjkdzh <ewelina_banach@interia.eu>
2022-05-31 00:04:02 +0200

    task3.2

--- app.py
+++ app.py
@@ -1,13 +1,40 @@
 import datetime
+import secrets
+
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+
+app = FastAPI()
+
+security = HTTPBasic()
+
+
+def get_current_username(credentials: HTTPBasicCredentials = Depends(security)):
+    correct_username = secrets.compare_digest(credentials.username, "stanleyjobson")
+    correct_password = secrets.compare_digest(credentials.password, "swordfish")
+    if not (correct_username and correct_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect email or password",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+    return credentials.username
+
+
+@app.get("/users/me")
+def read_current_user(username: str = Depends(get_current_username)):
+    return {"username": username}
 
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
 
 app = FastAPI()
 templates = Jinja2Templates("""<html>
         <h1>Welcome {{name}}! You are {{age}}</h1>
 </html>""")
+security = HTTPBasic()
 
 
 @app.get("/start", response_class=HTMLResponse)
@@ -18,7 +45,21 @@ def start():
     </html>
     """
 
-@app.post("/check/{name}/{date}", response_class=HTMLResponse)
-def check(name: str, date: str):
-    age = datetime.date.today() - datetime.date(date)
-    return templates.TemplateResponse({"name": name, "age": "16"})
+def get_current_username(credentials: HTTPBasicCredentials = Depends(security)):
+    correct_date = secrets.compare_digest(datetime.date.today() - datetime.date(credentials.username), 16)
+    if 16 > (correct_date):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect email or password",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+    return credentials.username
+
+@app.get("/check")
+def read_current_user(credentials: HTTPBasicCredentials = Depends(security)):
+    return {"username": credentials.username, "password": credentials.password}
+#
+# @app.get("/check", response_class=HTMLResponse)
+# def check(name: str, date: str):
+#     age = datetime.date.today() - datetime.date(date)
+#     return templates.TemplateResponse({"name": name, "age": "16"})
