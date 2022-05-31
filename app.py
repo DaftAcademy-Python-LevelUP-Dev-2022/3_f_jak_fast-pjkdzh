253196f243142db7509260c429851ea6e1f91028
pjkdzh <ewelina_banach@interia.eu>
2022-05-31 09:28:01 +0200

    task3.2

--- app.py
+++ app.py
@@ -1,7 +1,4 @@
-ns.
- 6  
-app.py
-@@ -1,57 +1,57 @@
+
 import datetime
 import secrets
 from fastapi import Depends, FastAPI, HTTPException, status
@@ -22,37 +19,75 @@ def start():
         <h1>The unix epoch started at 1970-01-01</h1>
     </html>
     """
-def get_current_username(credentials: HTTPBasicCredentials = Depends(security)):
-    correct_username = secrets.compare_digest(credentials.username, "tester")
-    correct_password = secrets.compare_digest(credentials.password, "1990-01-01")
-    correct_password1 = secrets.compare_digest(credentials.password, "2000-01-01")
-    if not (correct_password):
-        if not (correct_password1):
-            raise HTTPException(
-                status_code=status.HTTP_401_UNAUTHORIZED,
-                detail="Incorrect email or password",
-                headers={"WWW-Authenticate": "Basic"},
-            )
-    return credentials.username
+# def get_current_username(credentials: HTTPBasicCredentials = Depends(security)):
+#     correct_username = secrets.compare_digest(credentials.username, "tester")
+#     correct_password = secrets.compare_digest(credentials.password, "1990-01-01")
+#     correct_password1 = secrets.compare_digest(credentials.password, "2000-01-01")
+#     if not (correct_password):
+#         if not (correct_password1):
+#             raise HTTPException(
+#                 status_code=status.HTTP_401_UNAUTHORIZED,
+#                 detail="Incorrect email or password",
+#                 headers={"WWW-Authenticate": "Basic"},
+#             )
+#     return credentials.username
+# @app.post("/check", response_class=HTMLResponse)
+# def read_current_user(username: str = Depends(get_current_username)):
+#     return """ <html><h1>Welcome tester! You are 22</h1></html>"""
+
+def fetch_user_age(birth_date_str: str) -> int:
+    try:
+        birth_date = datetime.datetime.strptime(birth_date_str, "%Y-%m-%d")
+    except ValueError:
+        return -1
+
+    if birth_date > datetime.datetime.today():
+        return -1
+
+    return (datetime.datetime.today() - birth_date).days // 365
+
+
 @app.post("/check", response_class=HTMLResponse)
-def read_current_user(username: str = Depends(get_current_username)):
-    return """ <html><h1>Welcome tester! You are 22</h1></html>"""
+def login(request: Request, credentials: HTTPBasicCredentials = Depends(security)):
+    name = credentials.username
+    age = fetch_user_age(credentials.password)
 
+    if age < 16:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
 
-@app.put("/save/{string}", response_class=HTMLResponse, status_code=200)
+    return templates.TemplateResponse(
+        name='user_age_response.html.j2',
+        context={'request': request, 'name': name, 'age': age},
+        status_code=status.HTTP_200_OK
+    )
+
+@app.get("/info")
+def info(request: Request, format=None):
+    if not format:
+        pass
+
+    elif format.lower() == 'json':
+        return {'user_agent': request.headers.get('User-Agent')}
+
+    elif format.lower() == 'html':
+        return templates.TemplateResponse(
+            name='user_agent_response.html.j2',
+            context={'request': request, 'user_agent': request.headers.get('User-Agent')}
+        )
+
+    raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='Wrong format')
+
 @app.put("/save/{string}", status_code=200)
 def read_current_user(string: str):
     list.append(f"{string=}")
     return status.HTTP_200_OK
 
-@app.get("/save/{string}", response_class=HTMLResponse, status_code=404)
 @app.get("/save/{string}", status_code=404)
 def read_current_user(string: str):
     if f"{string=}" in list:
         return status.HTTP_301_MOVED_PERMANENTLY
     return status.HTTP_404_NOT_FOUND
 
-@app.delete("/save/{string}", response_class=HTMLResponse, status_code=404)
 @app.delete("/save/{string}", status_code=404)
 def read_current_user(string: str):
     for i in list:
